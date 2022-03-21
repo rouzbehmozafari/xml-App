4412d0e350233e45d7bc4efbf5aab177e93bbbec
rouzbehmozafari <rouzbeh.mozafari@yahoo.com>
2022-03-21 15:19:13 +0100

    final base

--- operatingSys/servieces.py
+++ operatingSys/servieces.py
@@ -1,17 +1,12 @@
-import os
-
 #writes the fetched xml to desired address
 def importXml(filePath,data):
-    fd = os.open(filePath,os.O_RDWR)
-    
-    # delete the old xml content
-    f = open(filePath, "a") # Create a blank file
-    f.seek(0)  # sets  point at the beginning of the file
-    f.truncate()  # Clear previous content
-    f.close() # Close file
-    
-    # writing new data
-    ret = os.write(fd,data.encode())
+    with open(filePath,'w') as f:
+        # delete the old xml content
+        f.seek(0)  # sets  point at the beginning of the file
+        f.truncate()  # Clear previous content
+        # writing new data
+        f.write(data)
+        f.close() # Close file
     
     # removine empty lines
     with open(filePath) as xmlfile:
